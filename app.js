be7b148739fb4f0178f426f1d17534e5bdfa4f29
Jen Noborikawa <jen.noborikawa@gmail.com>
2016-07-19 21:36:27 -0400

    end of tues class

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require("express"); 
 var morgan = require("morgan"); 
+var swig = require('swig');
 
 var app = express(); 
 app.use(morgan()); 
+
+app.set('view engine', 'html');
+app.engine('html', swig.renderFile);
 
 app.use(function(req, res, done){
 	// var status; 
@@ -12,6 +16,10 @@ app.use(function(req, res, done){
 	console.log(req.method + ' ' + req.originalUrl + ' '); 
 	done(); 
 }); 
+
+app.get('/', function(req, res){
+	res.render('index', { foo: 'bar', people: [{name: 'michael'}]});
+});
 
 app.use('/special', function(req, res, done){
 	res.send('you reached the special area');
